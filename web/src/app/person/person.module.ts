de85583c843db9f418aa41314b3462c0676a5f05
DanMakor <danielmcauliffe@outlook.com>
2021-04-19 16:10:31 +1000

    add create child and guardian

--- web/src/app/person/person.module.ts
+++ web/src/app/person/person.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildCreateComponent } from '../child/child-create/child-create.component';
 import { ChildModule } from '../child/child.module';
+import { GuardianCreateComponent } from '../guardian/guardian-create/guardian-create.component';
 import { GuardianModule } from '../guardian/guardian.module';
 import { SharedModule } from '../shared/shared.module';
 import { FamilyCreateComponent } from './family-create/family-create.component';
@@ -8,7 +10,9 @@ import { PersonHomeComponent } from './person-home/person-home.component';
 
 const routes: Routes = [
   { path: 'persons', component: PersonHomeComponent },
-  { path: 'persons/createFamily', component: FamilyCreateComponent}
+  { path: 'persons/createFamily', component: FamilyCreateComponent },
+  { path: 'persons/createGuardian', component: GuardianCreateComponent },
+  { path: 'persons/createChild', component: ChildCreateComponent }
 ];
 
 @NgModule({
