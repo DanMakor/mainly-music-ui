0d03042f6f55ec4e37974bd65e759ca8e8edddc3
DanMakor <danielmcauliffe@outlook.com>
2021-04-19 15:25:35 +1000

    remote address

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
       // Invalid ip
       console.log("Bad IP: " + req.socket.remoteAddress);
       const err = new Error("Bad IP: " + req.socket.remoteAddress);
-      next(validIps);
+      next({ validIps, remoteAddress: req.socket.remoteAddress });
   }
 });
 app.use(express.static(path.join(root, 'dist/')));
