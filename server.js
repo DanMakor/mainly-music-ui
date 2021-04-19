538724ce44f766ebed55aaab063757d151a3b68c
DanMakor <danielmcauliffe@outlook.com>
2021-04-19 15:12:57 +1000

    add ip block

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const root = './';
 const port = (process.env.PORT || 3000);
 const app = express();
 
+app.use((req, res, next) => {
+  let validIps = process.env.IPS?.split(',') || ['::12', '127.0.0.1']; // Put your IP whitelist in this array
+  console.log(validIps);
+  if(validIps.includes(req.socket.remoteAddress)){
+      // IP is ok, so go on
+      console.log("IP ok");
+      next();
+  }
+  else{
+      // Invalid ip
+      console.log("Bad IP: " + req.socket.remoteAddress);
+      const err = new Error("Bad IP: " + req.socket.remoteAddress);
+      next(err);
+  }
+});
 app.use(express.static(path.join(root, 'dist/')));
 app.get('*', (req, res) => {
   res.sendFile('dist/index.html', {root});
